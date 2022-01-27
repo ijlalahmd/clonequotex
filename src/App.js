64e2f31dc4c2d1f339671a8b54d897e189dfc9f9
ijlalahmed <ijlalahmed90@gmail.com>
2022-01-27 22:04:42 +0500

    init

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+//import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+import Chart from './components/GoogleChart'
+
+function App() {
+  return (
+    <div className="App">
+      <Chart/>
+      
+    </div>
+  );
+}
+
+export default App;
