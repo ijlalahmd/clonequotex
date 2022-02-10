23ecc79bbb50903a0a4a153b85778f1292495e27
zeeshan-shabbir <zeeshanshabbir186@gmail.com>
2022-02-10 18:22:06 +0500

    changeas

--- src/components/prediction/Predictcard.js
+++ src/components/prediction/Predictcard.js
@@ -68,63 +68,10 @@ function Predictcard() {
           </Button>
 
 
-<<<<<<< HEAD
         </Card.Body>
       </Card>
     </div>
   )
-=======
-    return (
-       
-        
-        <div className='card'>
-
-             <div href='#' className='depositbutton' spacing={2}>
-
-      
-      <button className='buttondeposit'> <span className='plusicon'>+</span> Deposit</button>
-      
-      <button className='buttonwithdrawal'><span className='plusicon1'>+</span> Withdrawal</button>
-      
- 
-
-
-        <Card >
-  <Card.Img variant="top"  />
-  <Card.Body>
-    <Card.Title className='abcclass'>Card Title</Card.Title>
-    <TextField className="textfieldabc"
-          id="outlined-required"
-          label="Time"
-          type="time"
-          text="17:08:00"
-    
-    /><br/><br/>
-    <TextField className="textfield1"
-          id="outlined-required"
-          label="Investment"
-          type="Money"
-          text="$"
-    
-    />
-<br/><br/>
-<Button className="button" variant="contained" color="primary" disableElevation>
-  Up<span className='classtodisable'>ZP--</span>
-</Button>
-<br/>
-<span className='payoutspan'> Your payout is :</span>
-<br/>
-<Button variant="contained" color="primary" disableElevation>
-  Down
-</Button>
-
-    
-  </Card.Body>
-</Card>
-</div>
-</div>
-    )
->>>>>>> ef2959175e604f92393fa12af6b1f64ea23077ba
 
 }
 
